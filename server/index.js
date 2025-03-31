596088bd589eca2c080f473ab9a47ebb3c4894be
Aadinath1234 <93751167+Aadinath1234@users.noreply.github.com>
2025-04-01 00:15:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-     origin:'http://localhost:5173',
+     origin:'https://mentalwellness-5xb5.onrender.com',
      credentials: true
 }
 
