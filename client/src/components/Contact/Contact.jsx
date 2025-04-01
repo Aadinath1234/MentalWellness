2260805b83a80be978d9fe4d040f3d2a4c20a5c8
Aadinath1234 <93751167+Aadinath1234@users.noreply.github.com>
2025-04-01 21:57:11 +0530

    Update Contact.jsx

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -2,9 +2,6 @@ import React,{useState, useEffect} from "react";
 import Navbar2 from "../Navbar/Navbar2.jsx";
 import Hero from "../Hero/Hero.jsx";
 import Hero2 from '../Hero/Hero2.jsx'; 
-
-// import BgVideo from '../../assets/earth-bg.mp4';
-// import BgVideo from '../../assets/bailvideo.mp4';
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -24,7 +21,7 @@ const Contact = () => {
           muted
           className="fixed right-0 top-0 h-[700px] w-full object-cover z-[-1]"
         >
-          {/* <source src={BgVideo} type="video/mp4" /> */}
+         
         </video>
         <Navbar2 />
         <Hero2 />
