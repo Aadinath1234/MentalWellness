da10a7b8bc3199956fd7f5a175bf10409635cb5a
Aadinath1234 <93751167+Aadinath1234@users.noreply.github.com>
2025-05-22 22:01:02 +0530

    Update Home.jsx

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,9 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../Navbar/Navbar.jsx";
 import Hero from "../Hero/Hero.jsx";
 import HeroCard from "../HeroCard/HeroCard.jsx";
+
+import BgVideo from "../../assets/videofront.mp4";
 import wave from "../../assets/wave Gif.gif";
 import Rapidscat from "../RapidScat/Rapidscat.jsx";
 import Satelite from "../Satellite/Satellite.jsx";
@@ -11,6 +13,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import Card from "../Card/Card.jsx";
 import Carousel from "../Carousel/Carousel.jsx";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   React.useEffect(() => {
@@ -19,16 +22,27 @@ const Home = () => {
       easing: "ease-in-out",
     });
   });
+  const navigate = useNavigate();
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+
+    if (storedUser) {
+      navigate("/HeroCard"); // Redirect to dashboard or home
+    } else {
+      navigate("/home"); // Redirect to login if not authenticated
+    }
+  }, [navigate]);
+
   return (
     <div className="overflow-x-hidden">
-      <div className="h-[700px] relative overflow-x-hidden">
+      <div className="h-[700px] relative">
         <video
           autoPlay
           loop
           muted
           className="fixed right-0 top-0 h-[700px] w-full object-cover z-[-1]"
         >
-          
+          <source src={BgVideo} type="video/mp4" />
         </video>
         <Navbar />
         <Hero />
