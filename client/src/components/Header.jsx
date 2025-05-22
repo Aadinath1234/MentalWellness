976d3871ab7e9d315ed0972d918e7e7d866266b9
Aadinath1234 <93751167+Aadinath1234@users.noreply.github.com>
2025-05-22 22:13:53 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { IoIosArrowDropdown } from 'react-icons/io';
-import { useSelector, useDispatch } from 'react-redux';
-import { setUser } from '../redux/userSlice';
-import { useNavigate } from 'react-router-dom';
-import toast from 'react-hot-toast';
-import axios from 'axios';
-import { API_END_POINT } from '../utils/constant';
+import React from "react";
+import { IoIosArrowDropdown } from "react-icons/io";
+import { useSelector, useDispatch } from "react-redux";
+import { setUser } from "../redux/userSlice";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { API_END_POINT } from "../utils/constant";
 
 const Header = () => {
-  const user = useSelector((store) => store.app.user);
+  // const user = useSelector((store) => store.app.user);
+  const user = JSON.parse(localStorage.getItem("user"));
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  console.log(user);
 
   const logoutHandler = async () => {
     try {
@@ -19,21 +22,29 @@ const Header = () => {
         toast.success(res.data.message);
       }
       dispatch(setUser(null)); // Reset user state in Redux
-      navigate('/');
+      navigate("/");
     } catch (error) {
-      toast.error('An error occurred during logout');
+      toast.error("An error occurred during logout");
       console.log(error);
     }
   };
+  const storedUser = localStorage.getItem("user");
 
+  if (storedUser) {
+    const user = JSON.parse(storedUser); // Parse JSON string to object
+    console.log(user.fullName); // 👉 Access fullName field
+  }
   return (
-    <div className='absolute flex w-[100vw] items-center justify-between px-6 bg-gradient-to-b from-black'>
+    <div className="absolute flex w-[100vw] items-center justify-between px-6 bg-gradient-to-b from-black">
       {user && (
         <div className="flex items-center">
-          <IoIosArrowDropdown size="24px" color='white' />
+          <IoIosArrowDropdown size="24px" color="white" />
           <h1 className="text-lg font-medium text-white">{user.fullName}</h1>
           <div className="ml-4">
-            <button onClick={logoutHandler} className="bg-red-800 text-white px-4 py-2">
+            <button
+              onClick={logoutHandler}
+              className="bg-red-800 text-white px-4 py-2"
+            >
               Logout
             </button>
           </div>
