db53fd5a69626817d00a029801847656d3bde43f
Aadinath1234 <93751167+Aadinath1234@users.noreply.github.com>
2025-05-22 22:11:04 +0530

    Update About.jsx

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,10 +1,7 @@
 import React,{useState, useEffect} from "react";
 import Navbar from "../Navbar/Navbar.jsx";
 import Hero from "../Hero/Hero.jsx";
-
-// import BgVideo from '../../assets/earth-bg.mp4';
-// import BgVideo from '../../assets/bailvideo.mp4'; 
-// import BgVideo from '../../assets/yoga.mp4'; 
+import BgVideo from '../../assets/videofront.mp4'; 
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -24,7 +21,7 @@ const Home = () => {
           muted
           className="fixed right-0 top-0 h-[700px] w-full object-cover z-[-1]"
         >
-          {/* <source src={BgVideo} type="video/mp4" /> */}
+          <source src={BgVideo} type="video/mp4" />
         </video>
         <Navbar />
         <Hero />
